82cfb98e808d26c89a19f3a7c707caa8f5674317
Vishal Dabas <84934458+Vishal921120@users.noreply.github.com>
2022-05-12 12:36:44 +0530

    Error correction

--- React/React app concepts/intro/src/Components/Small.js
+++ React/React app concepts/intro/src/Components/Small.js
@@ -7,7 +7,7 @@ import React from 'react';
 // }
 
 // Arrow function
-// This is name import in which we should have exactly the same name as of component while importing
+// This is name export in which we should have exactly the same name as of component while importing
 // so for importing this type we need to have it in curly brackets.
 export const Small = () =>  <div><h1>I am small from function component.</h1></div>
 
